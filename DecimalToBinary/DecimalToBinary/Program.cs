cd452ba8ba5163de407ecc554adf02488aafaf39
Mateusz Kus <Gustaweek@users.noreply.github.com>
2020-07-09 15:07:14 +0200

    Przeliczanie liczby na binarna

--- DecimalToBinary/DecimalToBinary/Program.cs
+++ DecimalToBinary/DecimalToBinary/Program.cs
@@ -6,7 +6,22 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj liczbe calkowita nie ujemna:");
+            int decimalNumber = int.Parse(Console.ReadLine());
+            if (decimalNumber > 0)
+            {
+                int remainder;
+                string result = string.Empty;
+                while (decimalNumber > 0)
+                {
+                    remainder = decimalNumber % 2;
+                    decimalNumber /= 2;
+                    result = remainder.ToString() + result;
+                }
+                Console.WriteLine("Liczba Binarna:  {0}", result);
+            }
+            else
+                Console.WriteLine("PODAJ LICZBE NIE UJEMNA");
         }
     }
 }
